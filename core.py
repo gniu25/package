6b7ed752380d8e16cb2ec071a1b3df690f92c793
Guiran Niu <32847629+gniu25@users.noreply.github.com>
2020-09-17 17:22:14 -0400

    Update core.py

--- core.py
+++ core.py
@@ -4,3 +4,5 @@ class MyClass(object):
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.logger.info("package initialized")
+    def get_5(self):
+    return 5
